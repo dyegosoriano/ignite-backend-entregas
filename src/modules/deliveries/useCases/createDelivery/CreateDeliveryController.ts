379b5005aa2cca092d1e497b34a44df9bbdb7f1f
Dyego Soriano <dyegodf2@gmail.com>
2022-02-08 19:29:21 -0300

    feat: save delivery on database

---
+++ src/modules/deliveries/useCases/createDelivery/CreateDeliveryController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+import { CreateDeliveryUseCase } from './CreateDeliveryUseCase'
+
+export class CreateDeliveryController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { item_name, id_client } = request.body
+
+    const createDeliveryUseCase = new CreateDeliveryUseCase()
+
+    const delivery = await createDeliveryUseCase.execute({ item_name, id_client })
+
+    return response.status(201).json(delivery)
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express'
 import { AuthenticateClientController } from './modules/account/useCases/authenticateClient/AuthenticateClientController'
 import { AuthenticateDeliverymanController } from './modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController'
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController'
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController'
 
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
+const createDeliveryController = new CreateDeliveryController()
 const createClientController = new CreateClientController()
-
 const routes = Router()
 
 routes.post('/client/authenticate', authenticateClientController.handle)
@@ -16,5 +17,7 @@ routes.post('/client', createClientController.handle)
 
 routes.post('/deliveryman', createDeliverymanController.handle)
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
+
+routes.post('/delivery', createDeliveryController.handle)
 
 export { routes }
