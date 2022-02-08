c2ff7c5900a4029aac26056b9459777cf687929e
Dyego Soriano <dyegodf2@gmail.com>
2022-02-08 20:10:44 -0300

    feat: create findAllAvailable use cases

---
+++ src/modules/deliveries/useCases/findAllAvailableUseCase/FindAllAvailableUseCase.ts
@@ -0,0 +1,11 @@
+import { prisma } from '../../../../database/prismaClient'
+
+class FindAllAvailableUseCase {
+  async execute() {
+    const deliveries = await prisma.deliveries.findMany({ where: { end_at: null } })
+
+    return deliveries
+  }
+}
+
+export { FindAllAvailableUseCase }
--- src/routes.ts
+++ src/routes.ts
@@ -4,13 +4,16 @@ import { AuthenticateClientController } from './modules/account/useCases/authent
 import { AuthenticateDeliverymanController } from './modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController'
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController'
+import { FindAllAvailableController } from './modules/deliveries/useCases/findAllAvailableUseCase/FindAllAvailableController'
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController'
 
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
+const findAllAvailableController = new FindAllAvailableController()
 const createDeliveryController = new CreateDeliveryController()
 const createClientController = new CreateClientController()
+
 const routes = Router()
 
 routes.post('/client/authenticate', authenticateClientController.handle)
@@ -20,5 +23,6 @@ routes.post('/deliveryman', createDeliverymanController.handle)
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
 
 routes.post('/delivery', ensureAuthenticateClient, createDeliveryController.handle)
+routes.get('/delivery/available', findAllAvailableController.handle)
 
 export { routes }
